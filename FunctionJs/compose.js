291e017912d3f0afeb4357ff01d7f9dd136f04dc
liuqiang <lqg@servyou.com.cn>
2016-12-23 18:26:17 +0800

    lqg

---
+++ FunctionJs/compose.js
@@ -0,0 +1,28 @@
+/**
+ * Created by lqg on 2016-12-22.
+ */
+import _ from 'underscore'
+import $ from 'jquery'
+
+/**
+ * 先分析一下之前写的一段代码
+    taxAttachBody[0].periodAmt = (_.reduce(countArray, (memo, num)=>memo + parseFloat(taxAttachBody[num].periodAmt), 0)).toFixed(2);
+    taxAttachBody[0].totalAmt = (_.reduce(countArray, (memo, num)=>memo + parseFloat(taxAttachBody[num].totalAmt), 0)).toFixed(2);
+
+ 有什么问题呢？
+    首先是一段代码写了两遍。如果多出有类似的功能，就要多写很多遍，不利于维护。而且这种很常见的功能应该抽到公有代码中去。
+
+ */
+
+/**
+ * 说明：按照自己的思路我会按照下面的方式封装。
+ * 计算collection指定顺序某个字段的合
+ * @param countArray
+ * @param collection
+ * @returns {Function}
+ */
+var getCount = function(countArray, collection){
+    return function(code){
+        return (countArray.reduce((memo, num)=>memo + parseFloat(collection[num][code]), 0)).toFixed(2);
+    }
+};
